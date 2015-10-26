940e8deceabef17fa3cd96fa45eabf73461b2d0c
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2015-10-26 11:17:54 -0300

    ajustes columnas

--- home.php
+++ home.php
@@ -110,6 +110,9 @@
 							<a href="<?php the_permalink();?>"><?php the_title();?></a>
 						</h3>
 						<p class="autor"><?php the_author( );?></p>
+						<div class="excerpt">
+							<?php the_excerpt();?>
+						</div>
 						<div class="bottom-meta">
 							
 							<p class="citas">
