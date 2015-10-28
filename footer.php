8a29f62ed26f834f83fb5d9d24aa3cf83a905aa4
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2015-10-27 21:41:39 -0300

    footer y offline version

--- footer.php
+++ footer.php
@@ -2,9 +2,32 @@
 <footer id="footer-sitio">
 	<div class="container">
 		<div class="footer-content">
-			<div class="footer-column">
-				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br /><a xmlns:cc="http://creativecommons.org/ns#" href="http://www.c80.cl" property="cc:attributionName" rel="cc:attributionURL">C80</a> está bajo una <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Licencia Creative Commons Atribución 4.0 Internacional</a>.
+			
+			<div class="footer-column info">
+				<img src="<?php bloginfo('template_url');?>/assets/img/logo-c80-footer.png" alt="<?php bloginfo('name');?>">
+				<p>Constitución: <br>Lo que dice, nos afecta</p>	
 			</div>
+
+			<div class="footer-column menu">
+				<ul>
+					<li>sobre el proyecto</li>
+					<li>¿cómo colaborar?</li>
+					<li>contacto</li>
+					<li>noticias</li>
+					<li>opinión</li>
+					<li>constitución</li>
+				</ul>
+			</div>
+
+			<div class="footer-column auspicios">
+				<p>Con el apoyo de:</p>
+				<img src="<?php bloginfo('template_url');?>/assets/img/logo-finlandia.png" alt="Embajada de Finlandia - Santiago Chile">
+			</div>
+
+			<div class="footer-licencia">
+				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="<?php bloginfo('template_url');?>/assets/img/cc.png" /></a><br /><a xmlns:cc="http://creativecommons.org/ns#" href="http://www.c80.cl" property="cc:attributionName" rel="cc:attributionURL">C80</a> está bajo una <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Licencia Creative Commons Atribución 4.0 Internacional</a>.
+			</div>
+
 		</div>
 	</div>
 </footer>
