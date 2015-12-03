06ef62c547235be188fd87f84fa973847e941391
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2015-12-03 15:22:09 -0300

    cambio newsletter

--- partials/inline-newsletter.php
+++ partials/inline-newsletter.php
@@ -1,7 +1,7 @@
 <div id="mc_embed_signup" class="inline">
 <form action="//c80.us11.list-manage.com/subscribe/post?u=af6bd471236cf2e72a950afac&amp;id=7ecd5c202c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-	<h2><i class="fa fa-envelope"></i> Entérate de las novedades del proyecto. ¡Suscríbete a nuestro boletín!</h2>
+	<h2><i class="fa fa-envelope"></i> Recibe las noticias, columnas y novedades de c80. ¡Suscríbete a nuestro boletín!</h2>
 <div class="mc-field-group">
 	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="deja tu e-mail">
 </div>
