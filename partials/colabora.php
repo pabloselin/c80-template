680d540b27cf82f1f7e4914cdd5cb4de74b6271e
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2015-12-03 16:23:29 -0300

    cambio color colabora

--- partials/colabora.php
+++ partials/colabora.php
@@ -1,4 +1,4 @@
-<div class="colabora alert alert-info">
+<div class="colabora alert">
 	<h5><i class="fa fa-hand-o-right"></i> ¿Quieres colaborar con una columna?</h5>
 	<p><a href="<?php echo get_permalink(516);?>">Revisa aquí nuestro protocolo sugerido</a></p>
 </div>
