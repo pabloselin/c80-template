79c882cb32b230fec74f6375e05be936db746716
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-04-01 09:40:31 -0300

    ajustes vista box c80

--- partials/vista-capitulo-box.php
+++ partials/vista-capitulo-box.php
@@ -1,26 +1,12 @@
 <article class="capitulo-constitucion-box">
+	<a href="<?php the_permalink();?>">
 	<div class="wrap">
 	<header class="header-capitulo">
-			<h1><a href="<?php the_permalink();?>" <span class="txt"><span class="titlename"><?php the_title();?></span> <br> <span class="capsubt"><?php echo c80t_captitle($post->ID);?></span> </span></a></h1>
+			<h1> <span class="txt"><span class="titlename"><?php the_title();?></span> <br> <span class="capsubt"><?php echo c80t_captitle($post->ID);?></span> </span></h1>
 		</header>	
-			<?php 
-				$args = array(
-					'post_type' => 'c80_cpt',
-					'numberposts' => 100,
-					'posts_per_page' => 100,
-					'post_parent' => $post->ID,
-					'orderby' => 'menu_order',
-					'order' => 'ASC'
-					);
 				
-				
-				$articulos = get_posts( $args );
-				$count = count($articulos);
-				echo $count . ' artículos';
-
-				?>
-				
-				<p><?php the_tags( 'Temas: ', ', ' );?></p>		
+				<!-- <p><?php the_tags( 'Temas: ', ', ' );?></p>		 -->
 				
 		</div>
+	</a>
 </article>
