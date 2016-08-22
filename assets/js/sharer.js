355ffbba2003a48f8b9444f3d4b598d736294d4b
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-08-22 15:44:59 -0300

    cambio url de compartidos facebook

--- assets/js/sharer.js
+++ assets/js/sharer.js
@@ -7,7 +7,7 @@ function singleCounter() {
         console.log('running counter in single');
 
         jQuery.getJSON('http://graph.facebook.com/?id=' + durl, function(json) {
-            fbshares = +json.shares || 0;
+            fbshares = +json.share.share_count || 0;
             nsh = roundNumber(parseInt(fbshares));
             jQuery('.sharer__facebook', sharer).append('<span>' + nsh + '</span>');
         });
