25dd15f13adcea6ab20b86ef954f65602edf3851
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-09-22 16:25:44 -0300

    theme version constant change

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //functions.php
 
-define( 'C80_THEME_VERSION', '0.1.0');
+define( 'C80_THEME_VERSION', '0.4.0');
 define( 'C80_TWITTER', 'proyectoC80');
 define( 'C80_FACEBOOK', 'https://www.facebook.com/proyectoC80/');
 
