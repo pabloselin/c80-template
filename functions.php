05915556c523d97a570d5acdc62ef76f0871b311
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-08-22 13:28:53 -0300

    modinfo

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //functions.php
 
-define( 'C80_THEME_VERSION', '0.5.10');
+define( 'C80_THEME_VERSION', '0.5.11');
 define( 'C80_TWITTER', 'proyectoC80');
 define( 'C80_FACEBOOK', 'https://www.facebook.com/proyectoC80/');
 define( 'C80_NOTFOUND', 1084);
