49aa85f38fc36b893333bd31a3f676d759a74b13
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2017-11-29 10:44:42 -0300

    ajustes tipograficos single

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //functions.php
 
-define( 'C80_THEME_VERSION', '0.5.45');
+define( 'C80_THEME_VERSION', '0.5.46');
 define( 'C80_TWITTER', 'proyectoC80');
 define( 'C80_FACEBOOK', 'https://www.facebook.com/proyectoC80/');
 define( 'C80_INSTAGRAM', 'https://www.instagram.com/proyectoc80/');
