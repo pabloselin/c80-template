8c1c2a2b3fe0b89661668aadded4abef6bcf3818
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2017-12-07 18:34:28 -0300

    more hover fixes

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //functions.php
 
-define( 'C80_THEME_VERSION', '1.0.5');
+define( 'C80_THEME_VERSION', '1.0.6');
 define( 'C80_TWITTER', 'proyectoC80');
 define( 'C80_FACEBOOK', 'https://www.facebook.com/proyectoC80/');
 define( 'C80_INSTAGRAM', 'https://www.instagram.com/proyectoc80/');
