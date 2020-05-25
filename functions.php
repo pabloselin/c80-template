6d8008da6601de965e0e171b66843f6739d3d904
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-05-25 10:48:43 -0400

    version push

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //functions.php
 
-define( 'C80_THEME_VERSION', '1.0.14');
+define( 'C80_THEME_VERSION', '1.0.15');
 define( 'C80_TWITTER', 'proyectoC80');
 define( 'C80_FACEBOOK', 'https://www.facebook.com/proyectoC80/');
 define( 'C80_INSTAGRAM', 'https://www.instagram.com/proyectoc80/');
