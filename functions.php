ca4269ea5fcc271fb70a0ef6daf1d98d8a9d7a50
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-05-29 14:26:55 -0400

    alineacion texto

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //functions.php
 
-define( 'C80_THEME_VERSION', '1.16.5');
+define( 'C80_THEME_VERSION', '1.16.6');
 define( 'C80_TWITTER', 'proyectoC80');
 define( 'C80_FACEBOOK', 'https://www.facebook.com/proyectoC80/');
 define( 'C80_INSTAGRAM', 'https://www.instagram.com/proyectoc80/');
