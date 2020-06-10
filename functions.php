671b1b486538425c17077cfe0ea558f9b8a1fe39
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-06-10 11:53:17 -0400

    ajuste boton comenzar

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //functions.php
 
-define( 'C80_THEME_VERSION', '1.16.23');
+define( 'C80_THEME_VERSION', '1.16.24');
 define( 'C80_TWITTER', 'proyectoC80');
 define( 'C80_FACEBOOK', 'https://www.facebook.com/proyectoC80/');
 define( 'C80_INSTAGRAM', 'https://www.instagram.com/proyectoc80/');
