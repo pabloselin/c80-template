6aef1e55fb781bb86f78e96e671e27ab68aba1c1
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-07-08 16:35:24 -0400

    fix link telegram

--- partials/timeline-sharer.php
+++ partials/timeline-sharer.php
@@ -3,5 +3,5 @@
 			<a class="share-link twitter" data-base="https://twitter.com/share?url=" data-additional="&via=proyectoC80"><i class="fa fa-twitter"></i></a>
 			<a class="share-link linkedin" data-base="http://www.linkedin.com/sharing/share-offsite/?url="><i class="fa fa-linkedin"></i></a>
 			<a class="share-link whatsapp" data-base="whatsapp://send?text="><i class="fa fa-whatsapp"></i></a>
-			<a class="share-link telegram" data-base="g://msg?text="><i class="fa fa-telegram"></i></a>
+			<a class="share-link telegram" data-base="tg://msg?text="><i class="fa fa-telegram"></i></a>
 		</div>
